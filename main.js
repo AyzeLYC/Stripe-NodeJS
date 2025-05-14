7c7e313495e31494e07391325bdde78b4ad9742d
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2025-05-14 20:54:39 +0200

    Created main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+import Stripe from "stripe";
+
+
+async function callPaymentIntentApi() {
+    
+    
+    
+};
+
+async function generatePaymentIntentUrl() {
+
+
+  
+};
+
+
+async function callCheckoutApi() {
+    
+    
+    
+};
+
+async function generateCheckoutPaymentPageUrl() {
+    
+    
+    
+};
